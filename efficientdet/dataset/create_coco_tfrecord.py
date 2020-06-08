04dbae23d88973952d84cec5077da682f9ec2f31
he.huang <he.huang@gpu-dev013.hogpu.cc>
2020-06-08 19:17:51 +0800

    fix import bug

--- efficientdet/dataset/create_coco_tfrecord.py
+++ efficientdet/dataset/create_coco_tfrecord.py
@@ -42,8 +42,8 @@ import PIL.Image
 
 from pycocotools import mask
 import tensorflow.compat.v1 as tf
-from dataset import label_map_util
-from dataset import tfrecord_util
+import label_map_util
+import tfrecord_util
 
 flags.DEFINE_boolean(
     'include_masks', False, 'Whether to include instance segmentations masks '
